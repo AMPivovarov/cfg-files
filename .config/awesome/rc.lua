89faf9c0a4c7a9334ffec9c005254a2a5c7132af
Aleksey Pivovarov <AMPivovarov@gmail.com>
2014-09-18 13:21:10 +0400

    zenconfig: use local one

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -15,7 +15,7 @@ local naughty = require('naughty')
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
 -- beautiful.init("/usr/share/awesome/themes/default/theme.lua")
-beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal  = "urxvtc"
